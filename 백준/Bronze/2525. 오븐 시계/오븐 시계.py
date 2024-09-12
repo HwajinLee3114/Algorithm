c01feccd486f57e572ae0cc14fd51be515b2bddb
Hwajin Lee <100420095+HwajinLee3114@users.noreply.github.com>
2024-09-12 13:44:25 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,6 +1,6 @@
 a, b = map(int, input().split())
 c = int(input())
 
-hour = a + ((b + c) // 60)
+hour = (a + ((b + c) // 60)) % 24
 min = (b + c) % 60
-print(hour - 24 if hour >= 24 else hour, min)
+print(hour, min)
