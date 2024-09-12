990d1afe8e834eb0ca012f6840fc7787e10be6ee
Hwajin Lee <100420095+HwajinLee3114@users.noreply.github.com>
2024-09-12 13:14:28 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,6 +1,4 @@
 a = int(input())
 b = int(input())
-if a > 0 and b > 0: print(1)
-elif a < 0 and b > 0: print(2)
-elif a < 0 and b < 0: print(3)
-else: print(4)
+if a * b > 0: print(1 if a > 0 else 3)
+else: print(4 if a > 0 else 2)
