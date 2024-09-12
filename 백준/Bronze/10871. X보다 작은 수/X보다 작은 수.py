df6332e019dcd8aa9e23060268ad6d362e49056a
Hwajin Lee <100420095+HwajinLee3114@users.noreply.github.com>
2024-09-12 17:09:21 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -2,7 +2,5 @@ import sys
 input = sys.stdin.readline
 n, x = map(int, input().split())
 a = input().split()
-_tmp = []
-for num in a:
-    if int(num) < x: _tmp.append(num)
-print(' '.join(_tmp))
+
+print(' '.join(num for num in a if int(num) < x))
