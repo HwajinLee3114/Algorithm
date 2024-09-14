8a4d66a69f0d3c16a5bcec6529c2d36ce3722e36
Hwajin Lee <100420095+HwajinLee3114@users.noreply.github.com>
2024-09-14 21:01:01 +0900

    [Bronze II] Title: 공 바꾸기, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -4,7 +4,5 @@ n, m = map(int, input().split())
 _tmp = list(range(1, n + 1))
 for _ in range(m):
     i, j = map(int, input().split())
-    tmp_num = _tmp[i - 1]
-    _tmp[i - 1] = _tmp[j - 1]
-    _tmp[j - 1] = tmp_num
+    _tmp[i - 1], _tmp[j - 1] = _tmp[j - 1], _tmp[i - 1]
 print(*_tmp)
