f8cbe7f2a49002c7f8b20c8154e9d928c275c890
Hwajin Lee <100420095+HwajinLee3114@users.noreply.github.com>
2024-09-19 20:44:36 +0900

    [Bronze II] Title: 만취한 상범, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/6359. 만취한 상범/만취한 상범.py
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.py
@@ -1,9 +1,11 @@
+import sys
+input = sys.stdin.readline
 t = int(input())
 for i in range(t):
     n = int(input())
-    rooms = [0] * (n + 1)
-    for j in range(2, n + 1):
-        for k in range(j, n + 1, j):
+    rooms = [0] * (n + 1)   # 처음엔 다 열려 있어
+    for j in range(2, n + 1):   # 라운드 돌기
+        for k in range(j, n + 1, j): # j 배수 일때 처리
             if rooms[k] == 0: rooms[k] = 1
             else: rooms[k] = 0
     print(rooms[1:].count(0))
