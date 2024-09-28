7c5ed4eb65c685d8970892ecf0d76925886f6c92
Hwajin Lee <jamong@kakao.com>
2024-09-28 21:32:40 +0900

    [Bronze I] Title: 롤 케이크, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3985. 롤 케이크/롤 케이크.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+L = int(input())
+N = int(input())
+cake = [0] * (L + 1)
+exp = exp_val = 0   # 기대
+real = real_val = 0 # 실제
+for i in range(1, N + 1):
+    p, k = map(int, input().split())
+    tmp = k - p + 1
+    if exp_val < tmp:
+        exp_val = tmp
+        exp = i
+    elif exp_val == tmp and exp > i:
+        exp = i
+    for j in range(p, k + 1):
+        if not cake[j]: cake[j] = i
+for i in range(1, N + 1):
+    tmp = cake.count(i)
+    if real_val < tmp:
+        real_val = tmp
+        real = i
+    elif real_val == tmp and real > i:
+        real = i
+print(exp)
+print(real)
