df9da13ca98fcf7db0c9843cc59984eb28afa4f2
Hwajin Lee <jamong@kakao.com>
2024-10-15 22:50:33 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,6 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+def switch_func(idx):
+    status[idx] = 0 if status[idx] == 1 else 1
+    return
+
 n = int(input())
 status = [0] + list(map(int, input().split()))
 st = int(input())
@@ -9,14 +13,14 @@ for _ in range(st):
     s, num = map(int, input().split())
     if s == 1:
         for i in range(num, n + 1, num):
-            status[i] = 0 if status[i] == 1 else 1
+            switch_func(i)
     elif s == 2:
-        status[num] = 0 if status[num] == 1 else 1
+        switch_func(num)
         for i in range(n // 2):
             if num + i > n or num - i < 1: break
             if status[num + i] == status[num - i]:
-                status[num + i] = 0 if status[num + i] == 1 else 1
-                status[num - i] = 0 if status[num - i] == 1 else 1
+                switch_func(num + i)
+                switch_func(num - i)
             else: break
 
 for i in range(1, len(status), 20):
